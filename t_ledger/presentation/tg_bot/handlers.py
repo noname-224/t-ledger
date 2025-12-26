186209408b64cf4ae4454ee126867de502a6faa1
Iliya Bound <bound.forcoding@gmail.com>
2025-12-26 16:27:35 +0300

    Переделал работу хэндлеров

--- t_ledger/presentation/tg_bot/handlers.py
+++ t_ledger/presentation/tg_bot/handlers.py
@@ -2,16 +2,14 @@ from aiogram import (
     Dispatcher,
     F,
 )
-from aiogram.filters import CommandStart
-from aiogram.types import (
-    Message,
-    CallbackQuery,
+from aiogram.filters import (
+    CommandStart,
+    Command,
 )
+from aiogram.types import Message
 
-from t_ledger.presentation.tg_bot.keyboards import (
-    start_window_buttons,
-    cancel_button,
-)
+from t_ledger.presentation.tg_bot.constants import BotOption
+from t_ledger.presentation.tg_bot.keyboards import main_keyboard
 from t_ledger.presentation.tg_bot.service import WindowLoaderService
 
 
@@ -21,25 +19,31 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """This handler receives messages with `/start` command"""
-    await message.answer(text=f"Выберите действие.", reply_markup=start_window_buttons())
-
-
-@dp.callback_query(F.data == "get_portfolio_allocation")
-async def handle_button(call: CallbackQuery) -> None:
-    await call.message.edit_text(
-        await WindowLoaderService.load_portfolio_allocation_info(), reply_markup=cancel_button()
+    text = (
+        "Привет!😁\n"
+        "Я могу предоставить информацию о твоем портфеле в Т-Инвестициях.\n\n"
+        "Чтобы получить интересующую тебя информацию — выбери нужный пункт в меню, "
+        "или отправь соответствующую команду."
     )
+    await message.answer(text=text, reply_markup=main_keyboard())
 
 
-@dp.callback_query(F.data == "get_risk_levels")
-async def handle_button(call: CallbackQuery) -> None:
-    await call.message.edit_text(
-        await WindowLoaderService.load_bonds_risk_levels(), reply_markup=cancel_button()
-    )
+@dp.message(F.text == BotOption.TOTAL_AMOUNT_PORTFOLIO)
+@dp.message(Command("total_amount_portfolio"))
+async def handle_button(message: Message) -> None:
+    await message.answer(
+        await WindowLoaderService.load_total_amount_portfolio())
 
 
-@dp.callback_query(F.data == "get_total_amount_portfolio")
-async def handle_button(call: CallbackQuery) -> None:
-    await call.message.edit_text(
-        await WindowLoaderService.load_total_amount_portfolio(), reply_markup=cancel_button()
-    )
+@dp.message(F.text == BotOption.PORTFOLIO_ALLOCATION)
+@dp.message(Command("portfolio_allocation"))
+async def handle(message: Message) -> None:
+    await message.answer(
+        await WindowLoaderService.load_portfolio_allocation_info())
+
+
+@dp.message(F.text == BotOption.BOND_RISK_LEVELS)
+@dp.message(Command("bond_risk_levels"))
+async def handle_button(message: Message) -> None:
+    await message.answer(
+        await WindowLoaderService.load_bonds_risk_levels())
