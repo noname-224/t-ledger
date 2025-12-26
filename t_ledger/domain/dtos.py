0b5ceaeb89c91e907c82432d9639f3761af0f3a0
Iliya Bound <bound.forcoding@gmail.com>
2025-12-26 12:15:12 +0300

    обновил pydantic модели

--- t_ledger/domain/dtos.py
+++ t_ledger/domain/dtos.py
@@ -1,21 +1,20 @@
 from decimal import Decimal
 
-from pydantic import BaseModel, ConfigDict, model_validator, Field, AliasPath
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+)
 from pydantic.alias_generators import to_camel
 
-from t_ledger.constants import Currency, AssetCategory, RiskLevel
+from t_ledger.domain.enums import (
+    BondType,
+    Currency,
+    RiskLevel,
+    InstrumentType,
+)
 
 
-class BaseDTOExtraAllow(BaseModel):
-    model_config = ConfigDict(
-        alias_generator=to_camel,
-        extra="allow",
-        validate_by_alias=True,
-        validate_by_name=True,
-    )
-
-
-class BaseDTOExtraIgnore(BaseModel):
+class BaseDTO(BaseModel):
     model_config = ConfigDict(
         alias_generator=to_camel,
         extra="ignore",
@@ -24,87 +23,68 @@ class BaseDTOExtraIgnore(BaseModel):
     )
 
 
-class QuantityDTO(BaseDTOExtraIgnore):
-    units: int
-    nano: int
+class Quantity(BaseDTO):
+    units: Decimal
+    nano: Decimal
 
     @property
-    def as_decimal(self) -> Decimal:
-        return Decimal(self.units) + Decimal(self.nano) / Decimal(1e9)
+    def value(self) -> Decimal:
+        return self.units + self.nano / Decimal(1e9)
 
 
-class MoneyDTO(BaseDTOExtraIgnore):
+class Money(BaseDTO):
     currency: Currency
-    units: int
-    nano: int
+    units: Decimal
+    nano: Decimal
 
     @property
-    def as_decimal(self) -> Decimal:
-        return Decimal(self.units) + Decimal(self.nano) / Decimal(1e9)
+    def amount(self) -> Decimal:
+        return self.units + self.nano / Decimal(1e9)
 
 
-class PositionDTO(BaseDTOExtraIgnore):
+class Position(BaseDTO):
     position_uid: str
     instrument_uid: str
     instrument_type: str
-    quantity: QuantityDTO
-    current_price: MoneyDTO
+    current_price: Money
+    quantity: Quantity
+    daily_yield: Money
+    current_nkd: Money | None = None
 
 
-class AmountByCategoryDTO(BaseDTOExtraIgnore):
-    category: str
-    amount: MoneyDTO
+class Instrument(BaseDTO):
+    type: InstrumentType
+    total_amount: Money
 
 
-class PortfolioDTO(BaseDTOExtraAllow):
-    total_amount_portfolio: MoneyDTO
-    positions: list[PositionDTO]
-    amounts_by_category: list[AmountByCategoryDTO] = []
-
-    @model_validator(mode="after")
-    def collect_total_amounts_by_category(self) -> "PortfolioDTO":
-        category_mapping = {
-            "totalAmountBonds": "bonds",
-            "totalAmountCurrencies": "currencies",
-            "totalAmountEtf": "etf",
-            "totalAmountFutures": "futures",
-            "totalAmountOptions": "options",
-            "totalAmountShares": "shares",
-            "totalAmountSp": "sp",
-        }
-
-        collected: list[AmountByCategoryDTO] = []
-        extra = self.model_extra or {}
-
-        for field_name, category in category_mapping.items():
-            if raw_amount := extra.get(field_name):
-                amount = MoneyDTO.model_validate(raw_amount)
-                collected.append(AmountByCategoryDTO(category=category, amount=amount))
-
-        self.amounts_by_category = collected
-        self.model_extra.clear()
-
-        return self
+class InstrumentOut(Instrument):
+    daily_yield: Decimal
+    alloc_percent: Decimal
 
 
-class CategoryInfoDTO(BaseDTOExtraIgnore):
-    amount: Decimal
-    category: AssetCategory
+class Portfolio(BaseDTO):
+    account_id: str
+    positions: list[Position]
+    total_amount_portfolio: Money
+    daily_yield: Money
+    instruments: list[Instrument]
+
+
+class Bond(BaseDTO):
+    asset_uid: str
+    bond_type: BondType
+    country_of_risk: str
+    country_of_risk_name: str
+    coupon_quantity_per_year: int
     currency: Currency
-    percentage: Decimal
+    name: str
+    nominal: Money
+    position_uid: str
+    risk_level: RiskLevel
+    sector: str
+    uid: str
 
 
-class BondInfoDTO(BaseDTOExtraIgnore):
-    uid: str = Field(validation_alias=AliasPath("instrument", "uid"))
-    name: str = Field(validation_alias=AliasPath("instrument", "name"))
-    risk_level: RiskLevel = Field(validation_alias=AliasPath("instrument", "riskLevel"))
-
-    @property
-    def risk_level_ru(self) -> str:
-        mapping = {
-            "RISK_LEVEL_UNSPECIFIED": "⚪️ не указан",
-            "RISK_LEVEL_LOW": "🟢 низкий",
-            "RISK_LEVEL_MODERATE": "🟡 средний",
-            "RISK_LEVEL_HIGH": "🔴 высокий",
-        }
-        return mapping.get(self.risk_level, "⚠️ неизвестно")
+class Allocation(BaseDTO):
+    active_instruments: list[InstrumentOut]
+    currency: Currency
