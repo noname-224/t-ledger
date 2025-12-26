e109145da3373ded7c9eeffaeb5fada65f588596
Iliya Bound <bound.forcoding@gmail.com>
2025-12-26 12:14:42 +0300

    поменял логику получения Portfolio и явно указываю ключ "instrument" при получении уровней риска

--- t_ledger/infra/client.py
+++ t_ledger/infra/client.py
@@ -1,13 +1,21 @@
+from typing import Any
+
 from aiohttp import ClientSession
 
 from t_ledger.config import settings
-from t_ledger.constants import (
+from t_ledger.domain.constants import instrument_types
+from t_ledger.domain.dtos import (
+    Bond,
+    Instrument,
+    Portfolio,
+    Position,
+)
+from t_ledger.domain.enums import (
     Method,
     Currency,
     Endpoint,
-    IdType,
+    InstrumentIdType,
 )
-from t_ledger.domain.entities import Position
 
 
 class TinkoffApiClient:
@@ -15,7 +23,7 @@ class TinkoffApiClient:
     def __init__(self, token: str) -> None:
         self.__token = token
 
-    def __get_headers(self) -> dict:
+    def __get_headers(self) -> dict[str: str]:
         return {"Authorization": f"Bearer {self.__token}"}
 
     async def __make_request(
@@ -25,21 +33,21 @@ class TinkoffApiClient:
         endpoint: str,
         headers: dict | None = None,
         json: dict | None = None,
-    ) -> dict:
+    ) -> dict[str: Any]:
         if headers is None:
             headers = self.__get_headers()
 
         if json is None:
             json = {}
 
-        result = await session.request(
+        result = await (await session.request(
             method=method,
             url=settings.tbank.base_url + endpoint,
             headers=headers,
             ssl=False,
             json=json,
-        )
-        return await result.json(encoding="utf-8")
+        )).json(encoding="utf-8")
+        return result
 
     async def __get_acc_id(self, session: ClientSession) -> str | None:
         result = await self.__make_request(
@@ -53,29 +61,45 @@ class TinkoffApiClient:
         except (KeyError, IndexError):
             return None
 
-    async def get_portfolio_data(self) -> dict:
+    async def get_portfolio(self, currency: Currency = Currency.RUB) -> Portfolio | None:
         async with ClientSession() as session:
             acc_id = await self.__get_acc_id(session)
             if acc_id is None:
                 return None
 
-            result = await self.__make_request(
+            data = await self.__make_request(
                 session=session,
                 method=Method.POST,
                 endpoint=Endpoint.GET_PORTFOLIO,
-                json={"accountId": acc_id, "currency": Currency.RUB},  # TODO возможность передачи валюты?
+                json={"accountId": acc_id, "currency": currency.upper()},
             )
-        return result
 
-    async def get_risk_level(self,  positions: list[Position]) -> list:
+        instruments = [
+            Instrument(
+                type=enum_type,
+                total_amount=data[json_field],
+            )
+            for json_field, enum_type in instrument_types.items()
+        ]
+
+        portfolio = Portfolio(
+            account_id=data["accountId"],
+            positions=data["positions"],
+            total_amount_portfolio=data["totalAmountPortfolio"],
+            daily_yield=data["dailyYield"],
+            instruments=instruments,
+        )
+        return portfolio
+
+    async def get_bonds(self,  positions: list[Position]) -> list[Bond]:
         async with ClientSession() as session:
-            result = [
-                await self.__make_request(
+            data = [
+                Bond.model_validate((await self.__make_request(
                     session=session,
                     method=Method.POST,
                     endpoint=Endpoint.GET_BOND_BY,
-                    json={"idType": IdType.UID, "id": position.instrument_uid}
-                )
+                    json={"idType": InstrumentIdType.UID, "id": position.instrument_uid},
+                ))["instrument"])
                 for position in positions
             ]
-        return result
+        return data
