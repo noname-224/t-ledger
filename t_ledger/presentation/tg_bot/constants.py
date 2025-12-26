ec2bfbc7235ddbbf1e89ef07adc6a037fc282c20
Iliya Bound <bound.forcoding@gmail.com>
2025-12-26 16:27:05 +0300

    добавил модуль для констант

---
+++ t_ledger/presentation/tg_bot/constants.py
@@ -0,0 +1,8 @@
+from enum import StrEnum
+
+
+class BotOption(StrEnum):
+    TOTAL_AMOUNT_PORTFOLIO = "Общая стоимость портфеля"
+    PORTFOLIO_ALLOCATION = "Аллокация портефеля"
+    FUTURE_BOND_PAYMENTS = ""
+    BOND_RISK_LEVELS = "Уровни риска облигаций"
