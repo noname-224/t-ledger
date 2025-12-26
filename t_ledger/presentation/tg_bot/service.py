04a5bbec7ce34b2e0d4bcdc45f979a87c1dbaf9e
Iliya Bound <bound.forcoding@gmail.com>
2025-12-26 12:16:26 +0300

    организовал красивый вывод

--- t_ledger/presentation/tg_bot/service.py
+++ t_ledger/presentation/tg_bot/service.py
@@ -1,27 +1,48 @@
 from aiogram import html
 
-from t_ledger.application.service import SuperService
+from t_ledger.application.service import InvestmentService
+from t_ledger.domain.constants import currency_to_sign
+from t_ledger.domain.enums import RiskLevel
 
 
 class WindowLoaderService:
-    service = SuperService()
+    service = InvestmentService()
 
     @classmethod
     async def load_portfolio_allocation_info(cls):
-        data = await cls.service.get_portfolio_allocation()
-        text = [
-            f"{i}. {d.category.capitalize()}: стоимость — {d.amount}, процент — {d.percentage}%"
-            for i, d in enumerate(data, 1)
+        alloc = await cls.service.get_portfolio_allocation()
+        sign = currency_to_sign.get(alloc.currency.upper(), "¤")
+
+        prepared_text = [
+            (
+                f"{html.bold(instr.type.upper())}\n"
+                f"💰 общая стоимость: {instr.total_amount.amount:.2f} {sign}\n"
+                f"📊 % аллокации: {instr.alloc_percent:.2f}%\n"
+                f"{"📈 доход" if instr.daily_yield >= 0 else "📉 убыток"}: {instr.daily_yield:.2f}"
+                f" {sign}"
+            )
+            for instr in alloc.active_instruments
         ]
 
-        return f"{html.bold("Аллокация портфеля:")}\n" + "\n".join(text)
+        return "\n\n".join(prepared_text)
 
     @classmethod
     async def load_bonds_risk_levels(cls):
-        data = await cls.service.get_positions_risk_levels()
-        text = [
-            f"{i}. {d.name}: {d.risk_level_ru}"
-            for i, d in enumerate(data, 1)
-        ]
+        data = await cls.service.get_bonds()
 
-        return f"{html.bold("Уровни риска облигаций:")}\n" + "\n".join(text)
+        mapping = {
+            RiskLevel.LOW: "🟢 Низкий уровень:",
+            RiskLevel.MODERATE: "🟡 Средний уровень:",
+            RiskLevel.HIGH: "🔴 Высокий уровень:",
+            RiskLevel.UNSPECIFIED: "⚪️ Уровень риска не указан:",
+        }
+
+        prepared_text = []
+        for risk_level, bonds in data.items():
+            if bonds:
+                prepared_text.append(f"{html.bold(mapping[risk_level])}")
+                for bond in bonds:
+                    prepared_text.append(f"    • {bond.name} ({bond.country_of_risk})")
+                prepared_text.append("")
+
+        return "\n".join(prepared_text)
