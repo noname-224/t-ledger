be787eac4bcac6d1e9aabb04871e1d95406135e6
Iliya Bound <bound.forcoding@gmail.com>
2025-12-28 21:36:28 +0300

    заменил некоторые литералы константами и перечислениями

--- t_ledger/presentation/tg_bot/constants.py
+++ t_ledger/presentation/tg_bot/constants.py
@@ -1,8 +1,25 @@
 from enum import StrEnum
 
 
-class BotOption(StrEnum):
+class BotMessageOption(StrEnum):
     TOTAL_AMOUNT_PORTFOLIO = "Общая стоимость портфеля"
     PORTFOLIO_ALLOCATION = "Аллокация портефеля"
-    FUTURE_BOND_PAYMENTS = ""
     BOND_RISK_LEVELS = "Уровни риска облигаций"
+    FUTURE_BOND_PAYMENTS = "Будущие выплаты по купонам"
+
+
+class BotCommandOption(StrEnum):
+    TOTAL_AMOUNT_PORTFOLIO = "total_amount_portfolio"
+    PORTFOLIO_ALLOCATION = "portfolio_allocation"
+    BOND_RISK_LEVELS = "bond_risk_levels"
+    FUTURE_BOND_PAYMENTS = "future_bond_payments"
+
+
+PLACE_HOLDER = "Выберите пункт меню."
+WINDOW_UNAVAILABLE_TEXT = "Это окно не доступно."
+
+currency_to_sign = {
+    "eur": "€",
+    "rub": "₽",
+    "usd": "$",
+}
--- t_ledger/presentation/tg_bot/handlers.py
+++ t_ledger/presentation/tg_bot/handlers.py
@@ -8,7 +8,11 @@ from aiogram.filters import (
 )
 from aiogram.types import Message
 
-from t_ledger.presentation.tg_bot.constants import BotOption
+from t_ledger.presentation.tg_bot.constants import (
+    BotCommandOption,
+    BotMessageOption,
+    WINDOW_UNAVAILABLE_TEXT,
+)
 from t_ledger.presentation.tg_bot.keyboards import main_keyboard
 from t_ledger.presentation.tg_bot.middlewares import AccessMiddleware
 from t_ledger.presentation.tg_bot.service import WindowLoaderService
@@ -30,22 +34,22 @@ async def command_start_handler(message: Message) -> None:
     await message.answer(text=text, reply_markup=main_keyboard())
 
 
-@dp.message(F.text == BotOption.TOTAL_AMOUNT_PORTFOLIO)
-@dp.message(Command("total_amount_portfolio"))
+@dp.message(F.text == BotMessageOption.TOTAL_AMOUNT_PORTFOLIO)
+@dp.message(Command(BotCommandOption.TOTAL_AMOUNT_PORTFOLIO))
 async def handle_button(message: Message) -> None:
     await message.answer(
         await WindowLoaderService.load_total_amount_portfolio())
 
 
-@dp.message(F.text == BotOption.PORTFOLIO_ALLOCATION)
-@dp.message(Command("portfolio_allocation"))
+@dp.message(F.text == BotMessageOption.PORTFOLIO_ALLOCATION)
+@dp.message(Command(BotCommandOption.PORTFOLIO_ALLOCATION))
 async def handle(message: Message) -> None:
     await message.answer(
         await WindowLoaderService.load_portfolio_allocation_info())
 
 
-@dp.message(F.text == BotOption.BOND_RISK_LEVELS)
-@dp.message(Command("bond_risk_levels"))
+@dp.message(F.text == BotMessageOption.BOND_RISK_LEVELS)
+@dp.message(Command(BotCommandOption.BOND_RISK_LEVELS))
 async def handle_button(message: Message) -> None:
     await message.answer(
         await WindowLoaderService.load_bonds_risk_levels())
