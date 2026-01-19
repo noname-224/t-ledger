699de383e431c32e6e4eb96735de332cb0b1d0fe
Iliya Bound <bound.forcoding@gmail.com>
2026-01-19 15:27:59 +0300

    test: Изменение теста

--- tests/unit/services/test_bond_risk_service.py
+++ tests/unit/services/test_bond_risk_service.py
@@ -5,7 +5,7 @@ from t_ledger.domain.enums.core import InstrumentType
 
 
 @pytest.mark.asyncio
-async def test_get_bonds_by_risks(mock_api_client, portfolio):
+async def test_get_bonds_by_risks(mock_api_client, mock_portfolio):
     service = BondRiskServiceImpl(mock_api_client)
 
     bonds_by_risks = await service.get_bonds_by_risks()
@@ -15,18 +15,17 @@ async def test_get_bonds_by_risks(mock_api_client, portfolio):
     for bonds_by_risk in bonds_by_risks:
         bonds += bonds_by_risk.bonds
 
+    assert bonds
+
     # 1. Из общего списка инструментов отфильтрованы только облигации
     bond_position_uids = {
         position.instrument_uid
-        for position in portfolio.positions
+        for position in mock_portfolio.positions
         if position.instrument_type == InstrumentType.BOND
     }
 
     assert all(map(lambda x: x.instrument_uid in bond_position_uids, bonds))
 
-    # 2. Каждая облигация имеет поле `quantity`
-    assert all(map(lambda x: x.quantity is not None, bonds))
-
-    # 3. Облигации сгрупированы корректно
+    # 2. Облигации сгрупированы корректно
     for bonds_by_risk in bonds_by_risks:
         assert all(map(lambda x: x.risk_level == bonds_by_risk.risk_level, bonds_by_risk.bonds))
